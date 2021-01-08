a38b30d37c9bbc706556422101a2c4691580961a
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-08 18:10:16 +0530

    Styling Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import './Login.css';
 
@@ -5,7 +6,7 @@ function Login() {
     return (
         <div className="spotify-login">
             <img className="spotify-logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="spotify-logo" />            
-            <button className="login-button">LOGIN with SPOTIFY</button>
+            <a href="" className="login-button">LOGIN with SPOTIFY</a>
         </div>
     )
 }
