24f1a3bf4e45afa947f1a38726e28e0c02057331
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-08 17:36:00 +0530

    ADD: Login Component

---
+++ src/components/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+    return (
+        <div className="spotify-login">
+            <img className="spotify-logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="spotify-logo" />            
+            <button className="login-button">LOGIN with SPOTIFY</button>
+        </div>
+    )
+}
+
+export default Login;
