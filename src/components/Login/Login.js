7f1550896abea8c23dff2b9853dc6ea823d4bf4d
vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-09 06:15:27 +0530

    GET: AccessToken

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,24 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Login.css';
-import {loginURL} from '../../utility/spotify';
+import {loginURL, getTokenFromURL} from '../../utility/spotify';
 
 function Login() {
+    const [token, setToken] = useState('');
+
+    useEffect(() => {
+        const hash = getTokenFromURL();
+        const token = hash.access_token;
+        if(token.length > 0){
+            setToken(token);
+        }
+        window.location.hash = "";
+    }, [])
+
     return (
         <div className="spotify-login">
             <img className="spotify-logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="spotify-logo" />            
-            <a href="{loginURL}" className="login-button">LOGIN with SPOTIFY</a>
+            <a href={loginURL} className="login-button">LOGIN with SPOTIFY</a>
         </div>
     )
 }
