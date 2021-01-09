02dfa6d7f5fccc93c875e3697f2214490b6924ec
vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-09 06:28:26 +0530

    UPDATE: App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Login from './components/Login/Login';
 import './App.css';
+import {getTokenFromURL} from './utility/spotify';
+import SpotifyWebAPI from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebAPI();
 
 function App() {
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+      const hash = getTokenFromURL();
+      const token = hash.access_token;
+      if(token){
+        setToken(token);
+        spotify.setAccessToken(token);
+      }
+      window.location.hash = "";
+  }, [])
   return (
     <div className="Spotify-app">
-      <Login />
+    {
+      token ? <h1>I have the token</h1> : <Login />
+    }
     </div>
   );
 }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,22 +1,12 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './Login.css';
-import {loginURL, getTokenFromURL} from '../../utility/spotify';
+import {loginURL} from '../../utility/spotify';
+import '../../styles/HelperStyles.css';
 
 function Login() {
-    const [token, setToken] = useState('');
-
-    useEffect(() => {
-        const hash = getTokenFromURL();
-        const token = hash.access_token;
-        if(token.length > 0){
-            setToken(token);
-        }
-        window.location.hash = "";
-    }, [])
-
     return (
-        <div className="spotify-login">
+        <div className="spotify-login flexColumn flexBetween flexAlignCenter">
             <img className="spotify-logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="spotify-logo" />            
             <a href={loginURL} className="login-button">LOGIN with SPOTIFY</a>
         </div>
