fffd2a1e66319567aaa72cf7aa6021f8bc5731d2
vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-09 05:48:03 +0530

    UPDATE: Login Component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,13 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import './Login.css';
+import {loginURL} from '../../utility/spotify';
 
 function Login() {
     return (
         <div className="spotify-login">
             <img className="spotify-logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="spotify-logo" />            
-            <a href="" className="login-button">LOGIN with SPOTIFY</a>
+            <a href="{loginURL}" className="login-button">LOGIN with SPOTIFY</a>
         </div>
     )
 }
