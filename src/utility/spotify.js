cd3d96078e26a78984ba200bc1e7493e46c7f616
vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-09 06:15:17 +0530

    Update spotify.js

--- src/utility/spotify.js
+++ src/utility/spotify.js
@@ -1,13 +1,24 @@
-export const authEndpoint = 'https://accounts.spotify.com/authorize';
-const redirectURL = 'https://localhost:3000/';
-const clientID = 'db9f9882a5254379927112b08cd15b51';
-
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+const clientID = "cff76213089a4e228206c8bf120d67cb";
+const redirectURL = "http://localhost:3000/";
 const scopes = [
-    "user-read-currently-playing",
-    "user-read-recently-played",
-    "user-read-playback-state",
-    "user-top-read",
-    "user-modify-playback-state",
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
 ];
 
-export const loginURL = `${authEndpoint}?client_id=${clientID}&redirect_uri=${redirectURL}&scope=${scopes.join('%')}&response_type=token&show_dialog=true`;
+export const getTokenFromURL = () => {
+    return window.location.hash
+      .substring(1)
+      .split("&")
+      .reduce((initial, item) => {
+        var parts = item.split("=");
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+  
+        return initial;
+      }, {});
+  };
+
+export const loginURL = `${authEndpoint}?client_id=${clientID}&redirect_uri=${redirectURL}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
