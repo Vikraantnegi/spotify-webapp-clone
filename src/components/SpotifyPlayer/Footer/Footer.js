5593027930b98dcee45f97aa5340fc7e25170e64
Vikraantnegi <43957569+Vikraantnegi@users.noreply.github.com>
2021-01-12 07:36:33 +0530

    UPDATE: Footer

--- src/components/SpotifyPlayer/Footer/Footer.js
+++ src/components/SpotifyPlayer/Footer/Footer.js
@@ -8,27 +8,78 @@ import { useStateValue } from "./StateProvider";
 function Footer({spotify}) {
     const [{ token, item, playing }, dispatch] = useStateValue();
     useEffect(() => {
-        spotify.getMyCurrentPlaybackState().then((song) => {
+        spotify.getMyCurrentPlaybackState().then((resp) => {
           dispatch({
             type: "SET_PLAYING",
-            playing: song.is_playing,
+            playing: resp.is_playing,
           });
     
           dispatch({
             type: "SET_ITEM",
-            item: song.item,
+            item: resp.item,
           });
         });
       }, [spotify]);
-    return (
+    
+    const PlayPause = () => {
+        if (playing) {
+          spotify.pause();
+          dispatch({
+            type: "SET_PLAYING",
+            playing: false,
+          });
+        } else {
+          spotify.play();
+          dispatch({
+            type: "SET_PLAYING",
+            playing: true,
+          });
+        }
+    };
+    
+    const Next = () => {
+        spotify.skipToNext();
+        spotify.getMyCurrentPlayingTrack().then((resp) => {
+          dispatch({
+            type: "SET_ITEM",
+            item: resp.item,
+          });
+          dispatch({
+            type: "SET_PLAYING",
+            playing: true,
+          });
+        });
+    };
+    
+    const Previous = () => {
+        spotify.skipToPrevious();
+        spotify.getMyCurrentPlayingTrack().then((resp) => {
+          dispatch({
+            type: "SET_ITEM",
+            item: resp.item,
+          });
+          dispatch({
+            type: "SET_PLAYING",
+            playing: true,
+          });
+        });
+    };
+    
+      return (
         <div className="player-footer flexRow flexBetween">
             <div className="song-details flexRow flexAlignCenter flexBetween">
-                <img src="" alt="album-cover" />
-                <div className="song-info flexColumn">
-                    <h4 className="song-name"></h4>
-                    <p className="singer"></p>
-                </div>
-
+                <img src={item?.album.images[0].url} alt="album-cover" />
+                { item ? (
+                    <div className="song-info flexColumn">
+                        <h4 className="song-name">{item.name}</h4>
+                        <p className="singer">{item.artists.map((artist) => artist.name).join(", ")}</p>
+                    </div>
+                ) : (
+                    <div className="song-info">
+                        <h4>No song is playing</h4>
+                        <p>...</p>
+                    </div>
+                )}
             </div>
             <div className="player-controls flexRow flexAlignCenter flexBetween">
                 <Shuffle className="player-control-toggle-icon" />
